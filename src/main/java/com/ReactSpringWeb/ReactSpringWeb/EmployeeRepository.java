4826248cf67988e801cbe9d9aa43c76b87f8de82
kazuakihiga <higak926@gmail.com>
2021-03-03 17:47:26 +0900

    add react

---
+++ src/main/java/com/ReactSpringWeb/ReactSpringWeb/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.ReactSpringWeb.ReactSpringWeb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
