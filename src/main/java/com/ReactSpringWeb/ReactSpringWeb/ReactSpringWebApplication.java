2dc61b00788f83263105b9f557a7ee96511d394f
kazuakihiga <higak926@gmail.com>
2021-03-03 16:12:41 +0900

    first commit

---
+++ src/main/java/com/ReactSpringWeb/ReactSpringWeb/ReactSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.ReactSpringWeb.ReactSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringWebApplication.class, args);
+	}
+
+}
